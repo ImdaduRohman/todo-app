b05bdfd70d034aec5cc13f07096606ce5ce941e0
ImdaduRohman <imdadu.ylabs1@gmail.com>
2023-04-28 20:12:38 +0700

    chore: initial setting

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import DashboardPage from './pages/DashboardPage'
+import DetailPage from './pages/DetailPage'
+// import Header from './components/Header'
+
+function App() {
+
+  return (
+    <>
+      {/* <Header /> */}
+      <Routes>
+        <Route path='/' element={<DashboardPage />} />
+        <Route path='/detail/:id' element={<DetailPage />} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
