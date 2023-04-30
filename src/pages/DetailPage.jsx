76d77ea6675d293a9f255abf43abbe5686ec5dd1
ImdaduRohman <imdadu.ylabs1@gmail.com>
2023-04-30 20:42:51 +0700

    fix: update edit title todo

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -22,7 +22,7 @@ export default function DetailPage() {
   const [isModalOpen, setIsModalOpen] = useState({ status: false, type: '' });
   const [selectedTodo, setSelectedTodo] = useState(null);
   const [selectedSort, setSelectedSort] = useState('');
-  console.log(selectedSort)
+  const [editTitle, setEditTitle] = useState(false);
   const { id } = useParams();
   const queryClient = useQueryClient();
 
@@ -135,19 +135,32 @@ export default function DetailPage() {
             <Link to='/' data-cy='todo-back-button' >
               <img src='/images/todo-back-button.svg' alt='todo-back-button' />
             </Link>
-            <div data-cy='todo-title'>
-              <input 
-                type='text' 
-                id='titleActivity' 
-                value={detailActivity?.title} 
-                onChange={handleInput} 
-                className='bg-[#F4F4F4] truncate w-fit outline-none focus:ring-2 focus:ring-[#16ABF8] p-2 rounded-sm'
-              />
-            </div>
+            <button type='button' onClick={() => setEditTitle(true)}>
+              {
+                editTitle ? (
+                  <input
+                    type='text'
+                    id='titleActivity' 
+                    value={detailActivity?.title} 
+                    onChange={handleInput} 
+                    onBlur={() => setEditTitle(false)}
+                    className='bg-[#F4F4F4] truncate w-fit outline-none focus:ring-2 focus:ring-[#16ABF8] p-2 rounded-sm'
+                  />
+                ) : (
+                  <div 
+                    data-cy='todo-title' 
+                    className='bg-[#F4F4F4] truncate w-fit outline-none focus:ring-2 focus:ring-[#16ABF8] p-2 rounded-sm'>
+                      {detailActivity?.title}
+                  </div>
+                )
+              }
+            </button>
             <label 
-              data-cy='todo-title-edit-button' 
+              data-cy='todo-title-edit-button'
+              type='button'
               htmlFor='titleActivity'
               className='hover:opacity-70'
+              onClick={() => setEditTitle(true)}
             >
               <img src='/images/todo-title-edit-button.svg' alt='todo-title-edit-button.svg' />
             </label>
